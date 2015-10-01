beb8db795f80460b849fbe1376532e16f2883b97
Elias Lund Lilja <elili552@li1-5.ida.liu.se>
2015-10-01 09:45:17 +0200

    added experiental code

--- src/xml_mutation.py
+++ src/xml_mutation.py
@@ -4,24 +4,36 @@ class Mutator:
     def __init__(self, xmlfile):
         self.xmlfile = xmlfile
         self.xmltree = parse(xmlfile)
+        self.xmlroot = self.xmltree.documentElement
 
     def find_nodes(self, node_type):
         node_list = []
         for node in self.xmltree.getElementsByTagName(node_type):
-            node_list.append(node)
-            print(node.getAttribute("name"))
+            if node.nodeType == Node.ELEMENT_NODE:
+                node_list.append(node)
+                print(node)
+            if node.hasChildNodes():
+                for child in node.childNodes:
+                    if child.nodeType == Node.ELEMENT_NODE:
+                        node_list.append(child)
+                        print(child)
 
-    def remove_node(self, node_type, node_name):
+    def remove_node(self, node_type, attribute_name = None):
+        # Find nodes matching the searched node type (e.g stub, harness etc)
         for node in self.xmltree.getElementsByTagName(node_type):
-            if node.childNodes is not None:
-                for children in node.getChildren:
-                    if node.getAttribute(node_name):
-                        xmltree.documentElement.removeChild(node)
-            else:
-                xmltree.documentElement.removeChild(node)
+            for child in node.childNodes:
+                if child.nodeType == Node.ELEMENT_NODE:
+                    print("Removing from parent node", child.parentNode.getAttribute("name"),",child", child)
+                    child.parentNode.removeChild(child)
+            #After all children are removed, remove the node itself
+            if node.getAttribute("name") == attribute_name or node.getAttribute("ref") == attribute_name or node.getAttribute("filename_rel") == attribute_name:
+                print("Removing from parent node", node.parentNode.tagName,",child", node)
+                node.parentNode.removeChild(node)
             
-        
-mutator = Mutator("testconstellations.xml")
+
+mutator = Mutator("testxml/testconstellations.xml")
+print("before removing")
 mutator.find_nodes("stub")
-mutator.remove_node("stub", "hardware_layer")
+mutator.remove_node("csu", "buttons")
+print("after removing")
 mutator.find_nodes("stub")        
