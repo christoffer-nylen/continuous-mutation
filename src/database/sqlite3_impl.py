cedd3c6464838c0799bba3e6bdb34e4826e8b420
Olle Kvarnström <olle+git@iix.se>
2015-10-15 15:18:41 +0200

    pep8 for sqlite3_impl.py

--- src/database/sqlite3_impl.py
+++ src/database/sqlite3_impl.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 """
-SQLite3 implementation module. 
+SQLite3 implementation module.
 Don't call this function directly, use db_handler to interact with database.
 
 
@@ -13,7 +13,9 @@ error_type: id, typ av fel
 errors: fel_id, typ_id
 """
 
+
 db = sqlite3.connect(":memory:")
+
 
 def createDB():
     cursor = db.cursor()
@@ -22,51 +24,48 @@ def createDB():
     cursor.execute("DROP TABLE IF EXISTS error_type")
     cursor.execute("DROP TABLE IF EXISTS errors")
 
-
     cursor.execute('''CREATE TABLE error_msg(
     id INTEGER PRIMARY KEY AUTOINCREMENT DEFAULT 0, msg TEXT UNIQUE )''')
-
 
     cursor.execute('''CREATE TABLE error_type(
     id INTEGER PRIMARY KEY AUTOINCREMENT DEFAULT 0, type TEXT UNIQUE )''')
 
-
     cursor.execute('''CREATE TABLE errors(
     msg_id INT, type_id INT,
-    FOREIGN KEY (msg_id) REFERENCES error_msg(id), 
+    FOREIGN KEY (msg_id) REFERENCES error_msg(id),
     FOREIGN KEY (type_id) REFERENCES error_type(id),
     PRIMARY KEY (msg_id, type_id))''')
 
-
     db.commit()
 
-    #db.close()
 
 def insert_error_msg(error_msg):
     cursor = db.cursor()
 
-    cursor.execute("INSERT OR IGNORE INTO error_msg  (msg) VALUES (?)", (error_msg, ))
-    #msg_id = cursor.lastrowid;
+    cursor.execute("INSERT OR IGNORE INTO error_msg  (msg) VALUES (?)",
+                   (error_msg, ))
     db.commit()
-    return cursor.lastrowid; #return msg_id
+    return cursor.lastrowid
 
-def insert_error_type(error_type):    
+
+def insert_error_type(error_type):
     cursor = db.cursor()
 
-    cursor.execute("INSERT OR IGNORE INTO error_type (type) VALUES (?)", (error_type, ))
+    cursor.execute("INSERT OR IGNORE INTO error_type (type) VALUES (?)",
+                   (error_type, ))
 
-        
     db.commit()
-    return cursor.lastrowid #retunrn type_id
-    
+    return cursor.lastrowid
+
+
 def insert_error(msg_id, type_id):
     cursor = db.cursor()
-    
-    cursor.execute("INSERT OR IGNORE INTO errors VALUES(?,?)", (msg_id, type_id,))
-    
+
+    cursor.execute("INSERT OR IGNORE INTO errors VALUES(?,?)",
+                   (msg_id, type_id,))
+
     db.commit()
 
-   
 
 def find(_error_msg):
     """
@@ -91,19 +90,20 @@ def get_type_id(_error_type):
     cursor.execute('''SELECT id FROM error_type
     WHERE type = (?)''', (_error_type,))
 
-    result  = cursor.fetchone()
+    result = cursor.fetchone()
 
     if result is not None:
         return result[0]
     return result
 
+
 def get_msg_id(_error_msg):
     cursor = db.cursor()
-    
+
     cursor.execute('''SELECT id FROM error_msg
     WHERE msg = (?)''', (_error_msg,))
 
-    result  = cursor.fetchone()
+    result = cursor.fetchone()
 
     if result is not None:
         return result[0]
