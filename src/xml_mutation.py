6d0027c1e370a494b573a9e4db3e047e01d4ef94
Dromedaren <dromedaren>
2015-10-15 11:40:27 +0200

    Commented and reworked function/variable names in the xml_mutation module

--- src/xml_mutation.py
+++ src/xml_mutation.py
@@ -4,13 +4,17 @@ import time
 
 class Mutator:
     def __init__(self, xmlfile):
-        """ Init the class objects parses an xml file and stores the root node"""
+        """ 
+        Init the Mutator class object
+        store a parsed given xml file
+        store linenumbers and the root node
+        """
         self.xmlfile = xmlfile
         self.xmltree = parse(xmlfile)
         self.xmlroot = self.xmltree.documentElement
-        self.modified_lines = []
-        self.linenumbers = {}
-        self.store_linenumbers(xmlfile)
+        self.xmlfile_lines_modified = []
+        self.xmlfile_linenumbers = {}
+        self.xmlfile.get_linenumbers(xmlfile)
 
     def NodeList(self):
         """Returns an empty list used for helper functions"""
@@ -22,9 +26,10 @@ class Mutator:
         return self._find_nodes_helper(self.xmlroot, node_type, node_attribute, self.NodeList())
 
     def _find_nodes_helper(self, parent, name, attribute, node_list):
-        """ Helper function that recursivly finds a node, given a name and/or attribute """
+        """
+        Helper function that recursivly finds a node, given a name and/or attribute
+        """
         for node in parent.childNodes:
-            #print(node)
             if node.nodeType == node.ELEMENT_NODE:
                 if node.tagName == name and attribute in \
                                 dict(node.attributes.items()).values():
@@ -35,11 +40,14 @@ class Mutator:
         return node_list                
 
     def remove_node(self, node_type, attribute_name = None):
-        found = self.find_nodes(node_type, attribute_name)
-        if found:
-            for node in found:
+        """
+        Remove a node matching the node_type, the optional attribute_name can be provided to narrow down possible candidates for the removal
+        """
+        nodes_for_removal = self.find_nodes(node_type, attribute_name)
+        if nodes_for_removal:
+            for node in nodes_for_removal:
                 try:
-                    self.modified_lines.append(str(self.linenumbers[self.convert_node_to_string(node)]))
+                    self.xmlfile_lines_modified.append(str(self.xmlfile_linenumbers[self.convert_node_to_string(node)]))
                     sibling = node.previousSibling
                     node.parentNode.removeChild(node)
                     sibling.nodeValue = sibling.nodeValue.strip()
@@ -47,10 +55,16 @@ class Mutator:
                     print("ERROR:", err)
 
     def convert_node_to_string(self, node):
+        """
+        Takes a xml node and returns a string representation of it
+        """
         str_node = node.toxml('utf-8').strip()
         return str_node.decode('utf-8').split(sep='\n')[0]
 
     def remove_attribute(self, node, name):
+        """
+        Remove an attribute specified by name from a given node
+        """
         try:
             node.removeAttribute(name)
         except NotFoundErr:
@@ -58,24 +72,32 @@ class Mutator:
         except AttributeError as err:
             print("ERROR:", err)
 
-    def store_linenumbers(self, filename):
+    def xmlfile_get_linenumbers(self, filename):
+        """
+        Sequentially read through the file storing each row tied with its linenumber
+        """
         with open(filename, "r") as xmlfile:
             for linenumber, line in enumerate(xmlfile):
-                self.linenumbers[line.strip()] = linenumber+1
+                self.xmlfile_linenumbers[line.strip()] = linenumber+1
 
     def generate_filename(self):
-        lines = ','.join(self.modified_lines)
-        file_name = time.strftime("%Y-%m-%d-%H:%M:%S") + "-lines_changed:" + lines
+        """ 
+        Generate a filename for a modified xml.
+        The filename will consist of a timestamp and the lines that have been modified, in order
+        """
+        lines_modified = ','.join(self.xmlfile_lines_modified)
+        file_name = time.strftime("%Y-%m-%d-%H:%M:%S") + "-lines_changed:" + lines_modified + '.xml'
         print("FILENAME: ", file_name)
         return file_name
             
     def write(self):
+        """ Write xml to disk using a generated filename """
         with open(self.generate_filename(), 'w', encoding='utf-8') as file_handle:
             self.xmlroot.writexml(file_handle)
 
 mutator = Mutator("testxml/testconstellations.xml")
 found = mutator.find_nodes("stub", "statistics")
-mutator.remove_attribute(found, "asdfa")
+mutator.remove_attribute(found[0], "asdfa")
 print(found)
 mutator.remove_node("stub", "control_unit")
 mutator.write()
