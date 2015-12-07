1c8554d538f21c0bf951e9d8eb559e8b2f3c0cef
Olle Kvarnström <olle+git@iix.se>
2015-12-07 11:25:27 +0100

    xml_mutator tests now work

--- continuous_mutation/test/xml_mutator.py
+++ continuous_mutation/test/xml_mutator.py
@@ -1,11 +1,17 @@
 import unittest
-from continuous_mutation.xml_mutation.xml_mutator import Mutator
+import os
 import xml.dom
+
+from continuous_mutation.xml_mutation.xml_mutator import Mutator
+
+path = os.path.dirname(__file__)
+if path == "":
+    path = "."
 
 class XmlMutationTests(unittest.TestCase):
 
     def setUp(self):
-        self.mutator = Mutator("../xml_mutation/testxml/testconstellations.xml")
+        self.mutator = Mutator(path + "/testconstellations.xml")
 
     def tearDown(self):
         self.mutator = None
@@ -13,17 +19,17 @@ class XmlMutationTests(unittest.TestCase):
     def test_node_list(self):
         list = self.mutator.node_list()
         self.assertEqual(0, len(list))
-    
+
     def test_find_nodes(self):
         all_nodes = self.mutator.find_nodes()
         self.assertEqual('<testconstellation name="user_interface_test">', self.mutator.convert_node_to_string(all_nodes[0]))
-    
+
     def test_generate_node_path(self):
         all_nodes = self.mutator.find_nodes()
         node_path = self.mutator.generate_node_path(all_nodes[2])
         self.assertEqual(self.mutator.convert_node_to_string(all_nodes[2]), node_path[0])
         self.assertEqual(self.mutator.convert_node_to_string(all_nodes[0]), node_path[len(node_path)-1])
-        
+
     def test_convert_node_to_string(self):
         all_nodes = self.mutator.find_nodes()
         str_node = self.mutator.convert_node_to_string(all_nodes[0])
@@ -33,8 +39,7 @@ class XmlMutationTests(unittest.TestCase):
         all_nodes = self.mutator.find_nodes()
         for index, mutated_node in enumerate(self.mutator.begin_mutation()):
             self.assertEqual(self.mutator.convert_node_to_string(all_nodes[index]), mutated_node[0])
-        
-    
-if __name__ == '__main__':
-    unittest.main()
+
+
+unittest.main()
 
